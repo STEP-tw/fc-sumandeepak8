b3627eadf800372e929693dac3921fce8a5f797c
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-01-16 13:12:23 +0530

    Added image and header using readFileSync.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
+const fs = require('fs');
 const app = (req, res) => {
+  let content = '';
+  console.log(req.url)
+  if (req.url == '/') {
+    content = fs.readFileSync('./src/flowerCatalog.html', 'utf-8');
+    res.write(content);
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+  if (req.url == '/freshorigins.jpg') {
+    content = fs.readFileSync('./images/freshorigins.jpg');
+    res.write(content);
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+  if (req.url == '/animated-flower-image-0021.gif') {
+    content = fs.readFileSync('./images/animated-flower-image-0021.gif');
+    res.write(content);
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+  res.write('wrong request');
   res.statusCode = 404;
   res.end();
 };
