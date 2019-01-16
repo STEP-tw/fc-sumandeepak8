8a23d8f93dee9f3f131cb6601510be20729573ed
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-01-16 22:03:19 +0530

    Conditionally modified app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
+
+
 const app = (req, res) => {
   let content = '';
   console.log(req.url)
-  if (req.url == '/') {
-    content = fs.readFileSync('./src/flowerCatalog.html', 'utf-8');
+
+  let filesPaths = {
+    '/': './src/flowerCatalog.html',
+    '/main.css': './src/main.css',
+  };
+
+  let imagesPaths = {
+    '/freshorigins.jpg': './images/freshorigins.jpg',
+    '/animated-flower-image-0021.gif': './images/animated-flower-image-0021.gif',
+  }
+
+  if (filesPaths[req.url]) {
+    content = fs.readFileSync(filesPaths[req.url], 'utf-8');
     res.write(content);
     res.statusCode = 200;
     res.end();
     return;
   }
-  if (req.url == '/freshorigins.jpg') {
-    content = fs.readFileSync('./images/freshorigins.jpg');
-    res.write(content);
-    res.statusCode = 200;
-    res.end();
-    return;
-  }
-  if (req.url == '/animated-flower-image-0021.gif') {
-    content = fs.readFileSync('./images/animated-flower-image-0021.gif');
+  if (imagesPaths[req.url]) {
+    content = fs.readFileSync(imagesPaths[req.url]);
     res.write(content);
     res.statusCode = 200;
     res.end();
