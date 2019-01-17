e2ebe968546e81267a32985088920afe529da98a
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-01-17 22:33:12 +0530

    Added public/guestBook

--- public/controller.js
+++ public/controller.js
@@ -6,3 +6,11 @@ const invisible = function () {
     gifDiv.style.visibility = 'visible';
   }, 1000);
 };
+
+const submit = function () {
+  let name = document.getElementById('name').value;
+  let comment = document.getElementById('comment').value;
+  var time = (new Date()).toLocaleTimeString();
+  document.getElementById('input').innerText = `${time} ${name}   ${comment}`;
+
+};
--- src/app.js
+++ src/app.js
@@ -6,31 +6,39 @@ const send = function (res, statusCode, content) {
   res.end();
 };
 
-const getPath = function (path) {
-  if (path == '/') return './public/flowerCatalog.html';
-  return './public' + path;
+const getPath = function (url) {
+  if (url == '/') return './public/flowerCatalog.html';
+  return './public' + url;
 };
 
 const getContent = function (req, res) {
   let path = getPath(req.url);
-  if (path) {
-    fs.readFile(path, (err, content) => {
-      send(res, 200, content);
-    });
-    return;
-  }
-  send(res, 404, 'wrong request');
+
+  fs.readFile(path, (err, content) => {
+    if (err) {
+      send(res, 404, 'File Not Found,404');
+      return;
+    }
+    send(res, 200, content);
+  });
 };
 
 const logRequest = function (req) {
+  console.log('req.headers', req.headers)
   console.log(req.method, req.url);
 };
 
 const app = (req, res) => {
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    res.write(data);
+  })
   logRequest(req);
   getContent(req, res);
 }
-
 
 // Export a function that can act as a handler
 
