49eb99c9dec3896c249449bf93dd8bb2173b4c54
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-01-18 12:46:08 +0530

    Removed app () from app.js and used app object of appData.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const AppData = require('./appData.js');
+const app = new AppData();
 
 const send = function (res, statusCode, content) {
   res.statusCode = statusCode;
@@ -13,7 +15,6 @@ const getPath = function (url) {
 
 const getContent = function (req, res) {
   let path = getPath(req.url);
-
   fs.readFile(path, (err, content) => {
     if (err) {
       send(res, 404, 'File Not Found,404');
@@ -23,23 +24,21 @@ const getContent = function (req, res) {
   });
 };
 
-const logRequest = function (req) {
-  console.log('req.headers', req.headers)
-  console.log(req.method, req.url);
+const logRequest = function (req, res, next) {
+  console.log('req.headers', req.headers);
+  console.log('method', req.method, req.url);
+  next();
 };
-
-const app = (req, res) => {
-  let data = '';
-  req.on('data', (chunk) => {
-    data += chunk;
-  });
-  req.on('end', () => {
-    res.write(data);
-  })
-  logRequest(req);
-  getContent(req, res);
-}
 
 // Export a function that can act as a handler
 
-module.exports = app;
+app.use(logRequest);
+app.get('/', getContent);
+app.get('/main.css', getContent);
+app.get('/controller.js', getContent);
+app.get('/guestBook.html', getContent);
+app.get('/images/freshorigins.jpg', getContent);
+app.get('/images/animated-flower-image-0021.gif', getContent);
+app.get('/favicon.ico', getContent);
+
+module.exports = app.handleRequest.bind(app);
